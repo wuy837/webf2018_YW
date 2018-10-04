cdf24da1676f6a827f94cc307d856112dc50f7f8
wuy837 <42848700+wuy837@users.noreply.github.com>
2018-10-04 19:04:18 -0400

    Add files via upload

---
+++ exercise_5/scripts.js
@@ -0,0 +1,48 @@
+$(document). ready(function(){
+	var bgImages = ["e51.jpg","e52.jpg","e53.jpg","e54.jpg", ];
+	var count = bgImages.length;
+	var ticker = 0;
+	var time = 0;
+	var date = new Date()
+	
+	$("#date").text(date);
+	
+	function screensaver(){
+		var url = "url(" + bgImages[ticker] + ")"
+		$("#screensaver").css("background-image",url);
+		
+		var tickerText = ticker + 1;
+		$("#ticker").text(tickerText);
+		ticker+=1;
+		if (ticker == count){
+			ticker = 0;
+		}
+	}
+	//screensaver();
+	$(document).click(function(){
+	
+    //screensaver();
+	})
+	setInterval(function(){
+		screensaver()
+	},2000)
+	$(document).mousemove(function(){
+		ticker=0;
+		time=0;
+		$("#screensaver").css("opacity",0)
+		$("#ticker").css("opacity",0)
+		
+	})
+	setInterval(function(){
+		time+=1;
+		if(time>2){
+			$("#screensaver").css("opacity",1)
+		$("#ticker").css("opacity",1)
+		
+		}
+		else{
+			
+		}
+	},1000)
+	
+})
